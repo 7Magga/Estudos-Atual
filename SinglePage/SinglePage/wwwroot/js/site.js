608854a4e718b25c187c46917d6b39d7f449faf2
Magga <matheus.dev.gabryel@outlook.com>
2022-06-03 19:59:29 -0300

    Novas Funções para o botão e manipulação da pagina.

--- SinglePage/SinglePage/wwwroot/js/site.js
+++ SinglePage/SinglePage/wwwroot/js/site.js
@@ -1,4 +1,53 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var currentList = {}
 
-// Write your JavaScript code.
+function createShoppingList() {
+    currentList.name = $('#shoppingListName').val();
+    currentList.items = new Array();
+    //Web service Call
+
+    $('#shoppingListTitle').html(currentList.name);
+    $('#shoppingListItems').empty();
+
+    $('#createListDiv').hide();
+    $('#shoppingListDiv').show();
+}
+
+function addItem() {
+    var newItem = {};
+    newItem.name = $("#itemName").val();
+    currentList.items.push(newItem);
+    console.info(currentList);
+    drawItems();
+}
+
+function drawItems() {
+    var $list = $('#shoppingListItems').empty();
+    for (var i = 0; i < currentList.items.length; i++) {
+        var currentItem = currentList.items[i];
+        var $li = $('<li>').html(currentItem.name).attr("id", "item_" + i);
+        var $deleteBtn = $("<button onclick='deleteItem(" + i + ")'>D</button>").appendTo($li);
+        var $checkBtn = $("<button onclick='checkItem(" + i + ")'>C</button>").appendTo($li);
+        $li.appendTo($list);
+    };
+
+}
+
+function checkItem(index) {
+
+    if ($("#item_" + index).hasClass("checked")) {
+        $("#item_" + index).removeClass("checked");
+    }
+    else
+    {
+        $("#item_" + index).addClass("checked");
+    }
+}
+
+function deleteItem(index) {
+    currentList.items.splice(index, 1);
+    drawItems();
+}
+
+$(document).ready(function () {
+    $('#shoppingListDiv').hide();
+})
