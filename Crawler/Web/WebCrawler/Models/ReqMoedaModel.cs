ae5d03ae5bce4c8c9ecd2cbc8777d40f4c4c145a
Magga <matheus.dev.gabryel@outlook.com>
2022-07-12 15:00:03 -0300

    Subida Single Page e MAIN do crawler web

---
+++ Crawler/Web/WebCrawler/Models/ReqMoedaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebCrawler.Models
+{
+    public class ReqMoedaModel
+    {
+        public object prev { get; set; }
+        public object next { get; set; }
+        public MoedaModel[] docs { get; set; }
+    }
+}
